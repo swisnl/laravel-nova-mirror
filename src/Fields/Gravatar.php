cdfcb4a6be014feb9965fc5c97f21e8f5500993f
Taylor Otwell <taylor@laravel.com>
2018-07-17 10:15:47 -0500

    wip

---
+++ src/Fields/Gravatar.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Laravel\Nova\Fields;
+
+class Gravatar extends Avatar
+{
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name = 'Avatar', $attribute = 'email', $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute ?? 'email', $resolveCallback);
+
+        $this->exceptOnForms();
+    }
+
+    /**
+     * Resolve the given attribute from the given resource.
+     *
+     * @param  mixed  $resource
+     * @param  string  $attribute
+     * @return mixed
+     */
+    protected function resolveAttribute($resource, $attribute)
+    {
+        $callback = function () use ($resource, $attribute) {
+            return 'https://www.gravatar.com/avatar/'.md5($resource->{$attribute}).'?s=300';
+        };
+
+        $this->preview($callback)->thumbnail($callback);
+    }
+}
