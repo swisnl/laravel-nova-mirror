58a6bc3d0c14f90733da62eadc26e2491b1cd4cc
Taylor Otwell <taylor@laravel.com>
2018-07-20 13:45:56 -0500

    wip

---
+++ src/NovaApplicationServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Laravel\Nova;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Cards\Help;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class NovaApplicationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->routes();
+
+        Nova::serving(function (ServingNova $event) {
+            $this->authorization();
+
+            Nova::resourcesIn(app_path('Nova'));
+            Nova::cards($this->cards());
+            Nova::tools($this->tools());
+        });
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+                ->withAuthenticationRoutes()
+                ->withPasswordResetRoutes();
+    }
+
+    /**
+     * Configure the Nova authorization services.
+     *
+     * @return void
+     */
+    protected function authorization()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+
+        Nova::auth(function ($request) {
+            return app()->environment('local') ||
+                   Gate::check('viewNova', [$request->user()]);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
