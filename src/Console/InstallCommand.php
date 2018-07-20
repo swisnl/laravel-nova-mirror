645f24b54dd61ff413454f3c4beb2d5e15f49291
Taylor Otwell <taylor@laravel.com>
2018-07-20 14:02:26 -0500

    wip

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -34,10 +34,26 @@ class InstallCommand extends Command
         $this->comment('Publishing Nova Service Provider...');
         $this->callSilent('vendor:publish', ['--tag' => 'nova-provider']);
 
+        $this->registerNovaServiceProvider();
+
         $this->comment('Generating User Resource...');
         $this->callSilent('nova:resource', ['name' => 'User']);
         copy(__DIR__.'/stubs/user-resource.stub', app_path('Nova/User.php'));
 
         $this->info('Nova scaffolding installed successfully.');
+    }
+
+    /**
+     * Register the Nova service provider in the application configuration file.
+     *
+     * @return void
+     */
+    protected function registerNovaServiceProvider()
+    {
+        file_put_contents(config_path('app.php'), str_replace(
+            "App\\Providers\EventServiceProvider::class,".PHP_EOL,
+            "App\\Providers\EventServiceProvider::class,".PHP_EOL."        App\Providers\NovaServiceProvider::class,".PHP_EOL,
+            file_get_contents(config_path('app.php'))
+        ));
     }
 }
