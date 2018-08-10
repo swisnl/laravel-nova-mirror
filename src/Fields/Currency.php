501873d437c4a4e26c41f0e99d0dc463d5ac0b08
Till Krüss <till@kruss.io>
2018-08-10 09:18:03 -0700

    add currency field

---
+++ src/Fields/Currency.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Laravel\Nova\Fields;
+
+class Currency extends Number
+{
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        $this->withMeta(['step' => '0.01']);
+
+        $this->displayUsing(function ($value) {
+            return money_format('%i', $value);
+        });
+    }
+
+    /**
+     * The format the field will used to be displayed in.
+     *
+     * @param  mixed  $step
+     * @return $this
+     */
+    public function format($format)
+    {
+        return $this->withMeta(['format' => $format]);
+    }
+}
