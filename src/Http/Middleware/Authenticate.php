1a965d714ca829894f0df890452ea49d258991dc
Taylor Otwell <taylor@laravel.com>
2018-08-13 13:12:25 -0500

    wip

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace Laravel\Nova\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as BaseAuthenticationMiddleware;
 use Laravel\Nova\Exceptions\AuthenticationException as NovaAuthenticationException;
@@ -9,18 +10,19 @@ use Laravel\Nova\Exceptions\AuthenticationException as NovaAuthenticationExcepti
 class Authenticate extends BaseAuthenticationMiddleware
 {
     /**
-     * Determine if the user is logged in to any of the given guards.
+     * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  array  $guards
-     * @return void
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
      *
      * @throws \Illuminate\Auth\AuthenticationException
      */
-    protected function authenticate($request, array $guards)
+    public function handle($request, Closure $next, ...$guards)
     {
         try {
-            return parent::authenticate($request, $guards);
+            return parent::handle($request, $next, ...$guards);
         } catch (AuthenticationException $e) {
             throw new NovaAuthenticationException('Unauthenticated.', $e->guards());
         }
