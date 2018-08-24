d77db24c2e991eb0370e77dd50ef694a2941c79e
David Hemphill <davidlee.hemphill@gmail.com>
2018-08-24 00:22:40 -0500

    wip

--- resources/js/components/Icons/Loader.vue
+++ resources/js/components/Icons/Loader.vue
@@ -17,7 +17,7 @@ export default {
         return (
             <svg
                 class="mx-auto block"
-                style={{ width: this.width }}
+                style={{ width: `${this.width}px` }}
                 viewBox="0 0 120 30"
                 xmlns="http://www.w3.org/2000/svg"
                 fill={this.fillColor}
