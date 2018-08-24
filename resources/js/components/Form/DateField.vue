7b581b8407ecab18c5b77b1e52ede4e4c2684532
David Hemphill <davidlee.hemphill@gmail.com>
2018-08-23 22:04:02 -0500

    wip

--- resources/js/components/Form/DateField.vue
+++ resources/js/components/Form/DateField.vue
@@ -5,7 +5,6 @@
                 class="w-full form-control form-input form-input-bordered"
                 :field="field"
                 :name="field.name"
-                :field="field"
                 :value="value"
                 dateFormat="Y-m-d"
                 :placeholder="placeholder"
