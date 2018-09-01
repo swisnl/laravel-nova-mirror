5bb33cd4d45226efeac12c8241834a142297efdc
Taylor Otwell <taylor@laravel.com>
2018-09-01 12:01:34 +0200

    wip

--- src/Fields/Text.php
+++ src/Fields/Text.php
@@ -12,33 +12,12 @@ class Text extends Field
     public $component = 'text-field';
 
     /**
-     * Indicates if the field should be shown as raw HTML in Vue.
-     *
-     * @var bool
-     */
-    public $asHtml = false;
-
-    /**
      * Display the field as raw HTML inside Vue.
      *
      * @return $this
      */
     public function asHtml()
     {
-        $this->asHtml = true;
-
-        return $this;
-    }
-
-    /**
-     * Prepare the field for JSON serialization.
-     *
-     * @return array
-     */
-    public function jsonSerialize()
-    {
-        return array_merge(parent::jsonSerialize(), [
-            'asHtml' => $this->asHtml,
-        ]);
+        return $this->withMeta(['asHtml' => true]);
     }
 }
