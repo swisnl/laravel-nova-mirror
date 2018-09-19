6e0ed121a34b3566097c677761529148d8487c31
David Hemphill <davidlee.hemphill@gmail.com>
2018-09-19 18:14:49 -0500

    fix 0.00 == null check in currency field

--- src/Fields/Currency.php
+++ src/Fields/Currency.php
@@ -24,7 +24,7 @@ class Currency extends Number
         parent::__construct($name, $attribute, $resolveCallback);
 
         $this->step('0.01')->displayUsing(function ($value) {
-            return $value ? money_format($this->format ?? '%i', $value) : null;
+            return !is_null($value) ? money_format($this->format ?? '%i', $value) : null;
         });
     }
 
