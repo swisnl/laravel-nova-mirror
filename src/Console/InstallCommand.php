e976dcae9951c8c1e6a1ffc1e7bb6ae4145ef504
Taylor Otwell <taylor@laravel.com>
2018-09-19 09:53:58 -0500

    wip

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,9 +3,12 @@
 namespace Laravel\Nova\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\DetectsApplicationNamespace;
 
 class InstallCommand extends Command
 {
+    use DetectsApplicationNamespace;
+
     /**
      * The name and signature of the console command.
      *
@@ -39,6 +42,8 @@ class InstallCommand extends Command
         $this->callSilent('nova:resource', ['name' => 'User']);
         copy(__DIR__.'/stubs/user-resource.stub', app_path('Nova/User.php'));
 
+        $this->setAppNamespace();
+
         $this->info('Nova scaffolding installed successfully.');
     }
 
@@ -49,10 +54,41 @@ class InstallCommand extends Command
      */
     protected function registerNovaServiceProvider()
     {
+        $namespace = str_replace_last('\\', '', $this->getAppNamespace());
+
         file_put_contents(config_path('app.php'), str_replace(
-            "App\\Providers\EventServiceProvider::class,".PHP_EOL,
-            "App\\Providers\EventServiceProvider::class,".PHP_EOL."        App\Providers\NovaServiceProvider::class,".PHP_EOL,
+            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL,
+            "{$namespace}\\Providers\EventServiceProvider::class,".PHP_EOL."        {$namespace}\Providers\NovaServiceProvider::class,".PHP_EOL,
             file_get_contents(config_path('app.php'))
+        ));
+    }
+
+    /**
+     * Set the proper application namespace on the installed files.
+     *
+     * @return void
+     */
+    protected function setAppNamespace()
+    {
+        $namespace = $this->getAppNamespace();
+
+        $this->setAppNamespaceOn(app_path('Nova/User.php'), $namespace);
+        $this->setAppNamespaceOn(app_path('Providers/NovaServiceProvider.php'), $namespace);
+    }
+
+    /**
+     * Set the namespace on the given file.
+     *
+     * @param  string  $file
+     * @param  string  $namespace
+     * @return void
+     */
+    protected function setAppNamespaceOn($file, $namespace)
+    {
+        file_put_contents($file, str_replace(
+            "App\\",
+            $namespace,
+            file_get_contents($file)
         ));
     }
 }
