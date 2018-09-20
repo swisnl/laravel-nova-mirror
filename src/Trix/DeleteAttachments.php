c9c40d13d8f3f1855cddbd82286363343d8b51bb
Taylor Otwell <taylor@laravel.com>
2018-09-20 14:25:55 -0500

    wip

--- src/Trix/DeleteAttachments.php
+++ src/Trix/DeleteAttachments.php
@@ -7,6 +7,24 @@ use Illuminate\Http\Request;
 class DeleteAttachments
 {
     /**
+     * The field instance.
+     *
+     * @var \Laravel\Nova\Fields\Trix
+     */
+    public $field;
+
+    /**
+     * Create a new class instance.
+     *
+     * @param  \Laravel\Nova\Fields\Trix  $field
+     * @return void
+     */
+    public function __construct($field)
+    {
+        $this->field = $field;
+    }
+
+    /**
      * Delete the attachments associated with the field.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -20,5 +38,7 @@ class DeleteAttachments
                 ->get()
                 ->each
                 ->purge();
+
+        return [$this->field->attribute => ''];
     }
 }
