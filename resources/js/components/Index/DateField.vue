89ae85cb9b04447486eb4c6ad3c08d2384aecfc0
Taylor Otwell <taylor@laravel.com>
2018-09-20 11:25:22 -0500

    wip

--- resources/js/components/Index/DateField.vue
+++ resources/js/components/Index/DateField.vue
@@ -1,9 +1,19 @@
 <template>
-    <span class="whitespace-no-wrap">{{ field.value }}</span>
+    <span class="whitespace-no-wrap">{{ formattedDate }}</span>
 </template>
 
 <script>
 export default {
     props: ['resourceName', 'field'],
+
+    computed: {
+        formattedDate() {
+          if (this.field.format) {
+              return moment(this.field.value).format(this.field.format)
+          }
+
+          return this.field.value
+        }
+    }
 }
 </script>
