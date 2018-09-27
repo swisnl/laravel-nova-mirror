fd04878beb7f627ca9bcd578a190aa5a491d44a9
Taylor Otwell <taylor@laravel.com>
2018-09-27 15:22:31 -0500

    formatting

---
+++ src/Fields/PasswordConfirmation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Laravel\Nova\Fields;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class PasswordConfirmation extends Password
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'password-field';
+
+    /**
+     * Create a new field.
+     *
+     * @param  string  $name
+     * @param  string|null  $attribute
+     * @param  mixed|null  $resolveCallback
+     * @return void
+     */
+    public function __construct($name, $attribute = null, $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        $this->onlyOnForms();
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  string  $requestAttribute
+     * @param  object  $model
+     * @param  string  $attribute
+     * @return void
+     */
+    protected function fillAttribute(NovaRequest $request, $requestAttribute, $model, $attribute)
+    {
+        return;
+    }
+}
