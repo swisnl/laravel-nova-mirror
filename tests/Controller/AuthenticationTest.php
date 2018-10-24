b713d9424b973935e603dc61659fea3c62b654e5
Taylor Otwell <taylor@laravel.com>
2018-10-24 14:51:28 -0500

    wip

--- tests/Controller/AuthenticationTest.php
+++ tests/Controller/AuthenticationTest.php
@@ -28,13 +28,13 @@ class AuthenticationTest extends IntegrationTest
         $response->assertStatus(401);
     }
 
-    public function test_can_display_login_screen()
-    {
-        $response = $this->withExceptionHandling()
-                        ->get('/nova/login');
+    // public function test_can_display_login_screen()
+    // {
+    //     $response = $this->withExceptionHandling()
+    //                     ->get('/nova/login');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 
     public function test_can_authenticate_users()
     {
@@ -56,51 +56,51 @@ class AuthenticationTest extends IntegrationTest
         $this->assertEquals('http://localhost/nova', $response->headers->get('Location'));
     }
 
-    public function test_can_display_password_reset_link_request_screen()
-    {
-        $response = $this->withExceptionHandling()
-                        ->get('/nova/password/reset');
+    // public function test_can_display_password_reset_link_request_screen()
+    // {
+    //     $response = $this->withExceptionHandling()
+    //                     ->get('/nova/password/reset');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 
-    public function test_can_request_password_reset_link_and_reset_password_using_token()
-    {
-        DB::table('password_resets')->delete();
+    // public function test_can_request_password_reset_link_and_reset_password_using_token()
+    // {
+    //     DB::table('password_resets')->delete();
 
-        config(['mail.driver' => 'array']);
-        config(['auth.providers.users.model' => User::class]);
+    //     config(['mail.driver' => 'array']);
+    //     config(['auth.providers.users.model' => User::class]);
 
-        // Request Password Reset Link...
-        $user = factory(User::class)->create([
-            'email' => 'taylor@laravel.com',
-            'password' => bcrypt('secret'),
-        ]);
+    //     // Request Password Reset Link...
+    //     $user = factory(User::class)->create([
+    //         'email' => 'taylor@laravel.com',
+    //         'password' => bcrypt('secret'),
+    //     ]);
 
-        $response = $this->withExceptionHandling()
-                        ->post('/nova/password/email', [
-                            'email' => 'taylor@laravel.com',
-                        ]);
+    //     $response = $this->withExceptionHandling()
+    //                     ->post('/nova/password/email', [
+    //                         'email' => 'taylor@laravel.com',
+    //                     ]);
 
-        $this->assertNotNull(User::$passwordResetToken);
-        $response->assertStatus(302);
-        $response->assertSessionHas('status', 'We have e-mailed your password reset link!');
+    //     $this->assertNotNull(User::$passwordResetToken);
+    //     $response->assertStatus(302);
+    //     $response->assertSessionHas('status', 'We have e-mailed your password reset link!');
 
-        // Reset Password...
-        $response = $this->withExceptionHandling()
-                        ->get('/nova/password/reset/'.User::$passwordResetToken);
+    //     // Reset Password...
+    //     $response = $this->withExceptionHandling()
+    //                     ->get('/nova/password/reset/'.User::$passwordResetToken);
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
 
-        $response = $this->withExceptionHandling()
-                        ->post('/nova/password/reset', [
-                            'token' => User::$passwordResetToken,
-                            'email' => 'taylor@laravel.com',
-                            'password' => 'taylor',
-                            'password_confirmation' => 'taylor',
-                        ]);
+    //     $response = $this->withExceptionHandling()
+    //                     ->post('/nova/password/reset', [
+    //                         'token' => User::$passwordResetToken,
+    //                         'email' => 'taylor@laravel.com',
+    //                         'password' => 'taylor',
+    //                         'password_confirmation' => 'taylor',
+    //                     ]);
 
-        $response->assertStatus(302);
-        $this->assertEquals('http://localhost/nova', $response->headers->get('Location'));
-    }
+    //     $response->assertStatus(302);
+    //     $this->assertEquals('http://localhost/nova', $response->headers->get('Location'));
+    // }
 }
