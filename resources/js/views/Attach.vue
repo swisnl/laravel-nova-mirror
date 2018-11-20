07099c181bff6b399091af1774ca33efc0be06d2
Marek Szymczuk <marek@osiemsiedem.com>
2018-11-20 19:09:10 +0100

    Don't encode special characters in the translation string

--- resources/js/views/Attach.vue
+++ resources/js/views/Attach.vue
@@ -85,7 +85,7 @@
                 <!-- Attach Button -->
                 <div class="bg-30 flex px-8 py-4">
                     <button dusk="attach-and-attach-another-button" type="button" @click="attachAndAttachAnother" class="ml-auto btn btn-default btn-primary mr-3">
-                        {{__('Attach &amp; Attach Another')}}
+                        {{__('Attach & Attach Another')}}
                     </button>
 
                     <button dusk="attach-button" class="btn btn-default btn-primary">
