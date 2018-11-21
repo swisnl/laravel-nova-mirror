9bb51d7ed2a572d447bc312ffce78d8cc0b59166
Taylor Otwell <taylor@laravel.com>
2018-11-21 07:53:00 -0600

    docblock

--- src/Filters/Option.php
+++ src/Filters/Option.php
@@ -9,25 +9,66 @@ use Laravel\Nova\ProxiesCanSeeToGate;
 
 class Option
 {
+    /**
+     * The option's label.
+     *
+     * @var string
+     */
     public $label;
 
+    /**
+     * The option's key.
+     *
+     * @var string
+     */
     public $key;
 
+    /**
+     * The option's truthy value.
+     *
+     * @var mixed
+     */
     public $trueValue;
 
+    /**
+     * The option's falsy value.
+     *
+     * @var mixed
+     */
     public $falseValue;
 
+    /**
+     * Create a new filter option instance.
+     *
+     * @param  string  $label
+     * @param  string  $key
+     * @return void
+     */
     public function __construct($label, $key = '')
     {
         $this->label = $label;
+
         $this->key = $key ?? str_replace(' ', '_', Str::lower($name));
     }
 
+    /**
+     * Create a new filter option instance.
+     *
+     * @param  string  $label
+     * @param  string  $key
+     * @return static
+     */
     public static function make($label, $key = '')
     {
         return new static($label, $key);
     }
 
+    /**
+     * Set the truthy value for the filter.
+     *
+     * @param  mixed  $value
+     * @return $this
+     */
     public function trueValue($value)
     {
         $this->trueValue = $value;
@@ -35,6 +76,12 @@ class Option
         return $this;
     }
 
+    /**
+     * Set the falsy value for the filter.
+     *
+     * @param  mixed  $value
+     * @return $this
+     */
     public function falseValue($value)
     {
         $this->falseValue = $value;
@@ -42,6 +89,11 @@ class Option
         return $this;
     }
 
+    /**
+     * Convert the filter option to an array.
+     *
+     * @return array
+     */
     public function toArray()
     {
         return [
