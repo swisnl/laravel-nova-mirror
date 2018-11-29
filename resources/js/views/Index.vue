cbfa7624f483725f3912c81e4387fc499e292262
David Hemphill <davidlee.hemphill@gmail.com>
2018-11-29 12:46:54 -0600

    fix issue that kept action resource counts from being updated correctly

--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -610,10 +610,6 @@ export default {
          * Get the count of all of the matching resources.
          */
         getAllMatchingResourceCount() {
-            if (this.resourceName == 'action-events') {
-                return
-            }
-
             Nova.request()
                 .get('/nova-api/' + this.resourceName + '/count', {
                     params: this.resourceRequestQueryString,
