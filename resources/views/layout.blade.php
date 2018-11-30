10f7239c9ab415d3840cf9225d9d8797f6957ea2
David Hemphill <davidlee.hemphill@gmail.com>
2018-11-30 00:09:58 -0600

    allow footer to be published alongside other views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -53,13 +53,7 @@
                 <div data-testid="content" class="px-view py-view mx-auto">
                     @yield('content')
 
-                    <p class="mt-8 text-center text-xs text-80">
-                        <a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
-                        <span class="px-1">&middot;</span>
-                        &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
-                        <span class="px-1">&middot;</span>
-                        v{{ Laravel\Nova\Nova::version() }}
-                    </p>
+                    @include('nova::partials.footer')
                 </div>
             </div>
         </div>
