361c16dc7361def9f134e5d7db2c8f6484f18790
Alex Bowers <bowersbros@gmail.com>
2018-12-04 00:21:12 +0000

    Batch queued actions in groups of 50

--- src/Actions/ActionEvent.php
+++ src/Actions/ActionEvent.php
@@ -199,7 +199,9 @@ class ActionEvent extends Model
             );
         });
 
-        static::insert($models->all());
+        $models->chunk(50)->each(function($models) {
+            static::insert($models->all());
+        });
 
         static::prune($models);
     }
