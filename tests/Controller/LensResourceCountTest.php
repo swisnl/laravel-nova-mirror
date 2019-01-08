81a50fd904ca3336b170ad817ff639644820f3c4
Marek Szymczuk <marek@osiemsiedem.com>
2019-01-08 22:16:59 +0100

    Fix counting resources with applied grouping

--- tests/Controller/LensResourceCountTest.php
+++ tests/Controller/LensResourceCountTest.php
@@ -3,6 +3,7 @@
 namespace Laravel\Nova\Tests\Controller;
 
 use Laravel\Nova\Nova;
+use Laravel\Nova\Tests\Fixtures\Role;
 use Laravel\Nova\Tests\Fixtures\User;
 use Laravel\Nova\Tests\IntegrationTest;
 use Laravel\Nova\Tests\Fixtures\IdFilter;
@@ -47,5 +48,22 @@ class LensResourceCountTest extends IntegrationTest
 
         $response->assertStatus(200);
         $this->assertEquals(1, $response->original['count']);
+    }
+
+    public function test_can_count_a_resource_with_grouping()
+    {
+        $roles = factory(Role::class, 2)->create();
+
+        factory(User::class, 3)
+           ->create()
+           ->each(function ($user) use ($roles) {
+                $user->roles()->sync($roles);
+            });
+
+        $response = $this->withExceptionHandling()
+                        ->getJson('/nova-api/users/lens/grouping-user-lens/count');
+
+        $response->assertStatus(200);
+        $this->assertEquals(3, $response->original['count']);
     }
 }
