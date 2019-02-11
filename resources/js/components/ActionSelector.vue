587283c24c5b524cc3015c67be57aa1d035eee7d
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2019-02-11 19:28:48 +0200

    CS

--- resources/js/components/ActionSelector.vue
+++ resources/js/components/ActionSelector.vue
@@ -139,7 +139,7 @@ export default {
                 this.executeAction()
             } else {
                 this.openConfirmationModal()
-            }    
+            }
         },
 
         /**
