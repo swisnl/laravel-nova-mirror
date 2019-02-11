dfa60f04027e0c65a811598dd2b58770b23dcd6e
David Hemphill <davidlee.hemphill@gmail.com>
2019-02-11 13:59:24 -0600

    wip

--- src/Http/Controllers/ForgotPasswordController.php
+++ src/Http/Controllers/ForgotPasswordController.php
@@ -37,7 +37,7 @@ class ForgotPasswordController extends Controller
             return (new MailMessage)
                 ->subject(__('Reset Password Notification'))
                 ->line(__('You are receiving this email because we received a password reset request for your account.'))
-                ->action(__('Reset Password'), url(config('app.url').route('nova.password.reset', $token, false)))
+                ->action(__('Reset Password'), url(config('nova.url').route('nova.password.reset', $token, false)))
                 ->line(__('If you did not request a password reset, no further action is required.'));
         });
     }
