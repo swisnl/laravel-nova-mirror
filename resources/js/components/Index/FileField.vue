4a6a3d26e22cda98722b8fb5c8115ef077a1f4b4
Benjamin Crozat <benjamincrozat@me.com>
2019-03-19 19:33:41 +0100

    Make FileField index thumbnail properly aligned (#367)
    Is it ok? It drives me crazy to see that gap that makes the thumbnail vertically misaligned with everything else on the row.

--- resources/js/components/Index/FileField.vue
+++ resources/js/components/Index/FileField.vue
@@ -4,7 +4,7 @@
             v-if="field.thumbnailUrl"
             :src="field.thumbnailUrl"
             style="object-fit: cover;"
-            class="rounded-full w-8 h-8"
+            class="align-bottom rounded-full w-8 h-8"
         />
         <span v-else>&mdash;</span>
     </p>
