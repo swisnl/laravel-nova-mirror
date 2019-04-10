d951ad24ea3806611fba3071a44fd836ae853eec
David Hemphill <davidlee.hemphill@gmail.com>
2019-04-10 14:25:00 -0500

    wip

--- src/Metrics/PartitionColors.php
+++ src/Metrics/PartitionColors.php
@@ -5,24 +5,45 @@ namespace Laravel\Nova\Metrics;
 class PartitionColors
 {
     /**
+     * The color array to use for the chart.
+     * 
      * @var array
      */
     public $colors;
 
+    /**
+     * The pointer to the current color in the chart array.
+     *
+     * @var integer
+     */
     private $pointer = 0;
 
-    public function __construct(array $colors = [])
+    /**
+     * Create a new instance.
+     *
+     * @param array $colors
+     */
+    public function __construct($colors = [])
     {
         $this->colors = $colors;
-
-        return $this;
     }
 
+    /**
+     * Get the color found at the given label key.
+     *
+     * @param string|int $label
+     * @return void
+     */
     public function get($label)
     {
         return $this->colors[$label] ?? $this->next();
     }
 
+    /**
+     * Return the next color in the color list.
+     *
+     * @return null|string
+     */
     protected function next()
     {
         return blank($this->colors) ? null :
