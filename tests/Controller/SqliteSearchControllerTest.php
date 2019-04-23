41ac9f7f00dd94e9d2240901afcad99b2ff23bb6
David Hemphill <davidlee.hemphill@gmail.com>
2019-04-23 11:31:53 -0500

    refactor search controller to test using mysql, postgres, and sqlite

---
+++ tests/Controller/SqliteSearchControllerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Laravel\Nova\Tests\Controller;
+
+use Laravel\Nova\Tests\IntegrationTest;
+
+class SqliteSearchControllerTest extends IntegrationTest
+{
+    use SearchControllerTests;
+}
